c28c0b7e6de75208fd28263c343f84a66a56859c
Cesar Niveyro <cehn17@gmail.com>
2025-04-11 15:10:47 -0300

    ADD: Recruiter - Post new Job

--- src/main/java/com/cehn17/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/cehn17/jobportal/controller/JobPostActivityController.java
@@ -1,5 +1,8 @@
 package com.cehn17.jobportal.controller;
 
+import com.cehn17.jobportal.entity.JobPostActivity;
+import com.cehn17.jobportal.entity.User;
+import com.cehn17.jobportal.services.JobPostActivityService;
 import com.cehn17.jobportal.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -8,15 +11,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
 
 @Controller
 public class JobPostActivityController {
 
     private final UserService userService;
+    private final JobPostActivityService jobPostActivityService;
 
     @Autowired
-    public JobPostActivityController(UserService userService) {
-        this.userService = userService;
+    public JobPostActivityController(UserService usersService, JobPostActivityService jobPostActivityService) {
+        this.userService = usersService;
+        this.jobPostActivityService = jobPostActivityService;
     }
 
     @GetMapping("/dashboard")
@@ -33,5 +41,25 @@ public class JobPostActivityController {
         model.addAttribute("user", currentUserProfile);
 
         return "dashboard";
+    }
+
+    @GetMapping("/dashboard/add")
+    public String addJobs(Model model) {
+        model.addAttribute("jobPostActivity", new JobPostActivity());
+        model.addAttribute("user", userService.getCurrentUserProfile());
+        return "add-jobs";
+    }
+
+    @PostMapping("/dashboard/addNew")
+    public String addNew(JobPostActivity jobPostActivity, Model model) {
+
+        User user = userService.getCurrentUser();
+        if (user != null) {
+            jobPostActivity.setPostedById(user);
+        }
+        jobPostActivity.setPostedDate(new Date());
+        model.addAttribute("jobPostActivity", jobPostActivity);
+        jobPostActivityService.addNew(jobPostActivity);
+        return "redirect:/dashboard";
     }
 }
---
+++ src/main/java/com/cehn17/jobportal/services/JobPostActivityService.java
@@ -0,0 +1,19 @@
+package com.cehn17.jobportal.services;
+
+import com.cehn17.jobportal.entity.JobPostActivity;
+import com.cehn17.jobportal.repository.JobPostActivityRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobPostActivityService {
+
+    private final JobPostActivityRepository jobPostActivityRepository;
+
+    public JobPostActivityService(JobPostActivityRepository jobPostActivityRepository) {
+        this.jobPostActivityRepository = jobPostActivityRepository;
+    }
+
+    public JobPostActivity addNew(JobPostActivity jobPostActivity) {
+        return jobPostActivityRepository.save(jobPostActivity);
+    }
+}
